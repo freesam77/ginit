f7984f221d84f58788aea13539e3d51ebe505c8e
Samuel Razali <samuel.razali@qantas.com.au>
2020-03-09 17:22:29 +1100

    1.0

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
+#!/usr/bin/env node
 const chalk = require('chalk');
 const clear = require('clear');
 const figlet = require('figlet');
 const files = require('./lib/files');
 const inquirer = require('./lib/inquirer');
 const github = require('./lib/github');
+const repo = require('./lib/repo');
 
 clear();
 
@@ -19,15 +21,57 @@ if(files.directoryExists('.git')){
 }
 
 const run = async () => {
-    const credentials = await inquirer.askGithubCredentials();
-    console.log(credentials)
+    // const credentials = await inquirer.askGithubCredentials();
+    // console.log(credentials)
 
+    // let token = github.getStoredGithubToken();
+    // if (!token){
+    //     token = await github.getPersonalAccessToken()
+    // }
+
+    // console.log(token)
+
+    try{
+        // Retrieve & Set Authentication Token
+        const token = await getGithubToken();
+        github.githubAuth(token);
+
+        // Create remote repo
+        const url = await repo.createRemoteRepo();
+
+        // Create .gitignore file
+        await repo.createGitignore();
+
+        // Set up local repo and push to remote
+        await repo.setupRepo(url);
+
+        console.log(chalk.green('All done!'))
+    } catch(err) {
+        if(err){
+            switch (err.status) {
+                case 401:
+                    console.log(chalk.red('Couldn\'t log you in. Please provide correct credentials/token. '))
+                    break;
+                case 422:
+                    console.log(chalk.red('There is already a remote repo with the same name!'));
+                    break;
+                default:
+                    console.log(chalk.red(err))
+            }
+        }
+    }   
+}
+
+const getGithubToken = async() => {
+    // Fetch token from config store
     let token = github.getStoredGithubToken();
-    if (!token){
-        token = await github.getPersonalAccessToken()
+    if(token){
+        return token
     }
 
-    console.log(token)
+    // No token found, use cred to access Github account
+    token = await github.getPersonalAccessToken();
+    return token;
 }
 
 run()
